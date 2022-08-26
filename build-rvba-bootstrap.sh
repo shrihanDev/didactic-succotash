0e3b7fe21d0a2a1b5f1e373e29c11e5068facb51
shrihanDev <shrihankp198725@gmail.com>
2022-08-26 06:35:41 +0530

    mod rvba script
    Signed-off-by: shrihanDev <shrihankp198725@gmail.com>

--- build-rvba-bootstrap.sh
+++ build-rvba-bootstrap.sh
@@ -14,21 +14,24 @@ mkdir logs
   log "Checking out PR#10540"
   gh pr checkout 10540
 
-#  log "Patching setup scripts"
-#  sed -i '/venv/d' scripts/setup-ubuntu.sh
-#  sed -i 's/openjdk-18/openjdk-17/g' scripts/setup-ubuntu.sh
-#  sed -i 's#cmdline-tools/bin#cmdline-tools/latest/bin#g' scripts/setup-android-sdk.sh
+  log "Patching setup scripts"
+  sed -i '/venv/d' scripts/setup-ubuntu.sh
+  sed -i 's/openjdk-18/openjdk-17/g' scripts/setup-ubuntu.sh
+  sed -i 's#cmdline-tools/bin#cmdline-tools/latest/bin#g' scripts/setup-android-sdk.sh
 
-#  log "Running setup scripts"
-#  ./scripts/setup-ubuntu.sh
-#  ./scripts/setup-android-sdk.sh
+  log "Running setup scripts"
+  ./scripts/setup-ubuntu.sh
+  ./scripts/setup-android-sdk.sh
+
+  log "Restoring the setup scripts"
+  git reset --hard HEAD scripts/setup-ubuntu.sh
+  git reset --hard HEAD scripts/setup-android-sdk.sh
 
   log "Changing package ID"
   sed -i 's/TERMUX_APP_PACKAGE="com.termux"/TERMUX_APP_PACKAGE="com.reisxd.rvba"/g' scripts/properties.sh
 
-# log "Patching build-bootstrap.sh"
-# curl -sLo scripts/bb.sh.patch https://github.com/termux/termux-packages/commit/f6fa7e932760e4edd67c155ca52ece3a8776d2c5.patch
-# patch -i scripts/bb.sh.patch scripts/build-bootstrap.sh
+ log "Patching build-bootstrap.sh"
+ curl -sLo scripts/build-bootstraps.sh https://raw.githubusercontent.com/termux/termux-packages/f6fa7e932760e4edd67c155ca52ece3a8776d2c5/scripts/build-bootstraps.sh
 
   log "Building bootstrap for aarch64"
   ./scripts/run-docker.sh ./scripts/build-bootstraps.sh --add nodejs-lts,openjdk-17 --architectures aarch64
