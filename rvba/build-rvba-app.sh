7b3f370baa09cbe3d9b9cb76ad9a9baeddb83d7d
shrihanDev <shrihankp198725@gmail.com>
2022-08-28 17:29:16 +0530

    mod rvba <all>
    Signed-off-by: shrihanDev <shrihankp198725@gmail.com>

--- rvba/build-rvba-app.sh
+++ rvba/build-rvba-app.sh
@@ -4,34 +4,6 @@ set -euo pipefail
 log(){
   echo -e "\e[34m[**] $1\e[0m" >&2
 }
-mkdir logs
-
-{
-  log "Cloning termux-packages"
-  git clone https://github.com/termux/termux-packages --depth=1 --no-tags
-  cd termux-packages
-
-  log "Checking out PR#10540"
-  gh pr checkout 10540
-
-  log "Changing package ID"
-  sed -i 's/TERMUX_APP_PACKAGE="com.termux"/TERMUX_APP_PACKAGE="com.reisxd.rvba"/g' scripts/properties.sh
-
-  #log "Patching setup scripts"
-  #sed -i '/venv/d' scripts/setup-ubuntu.sh
-  #sed -i 's/openjdk-18/openjdk-17/g' scripts/setup-ubuntu.sh
-  #sed -i 's#cmdline-tools/bin#cmdline-tools/latest/bin#g' scripts/setup-android-sdk.sh
-
-  #log "Running setup scripts"
-  #./scripts/setup-ubuntu.sh
-  #./scripts/setup-android-sdk.sh
-
-  log "Building bootstrap for aarch64"
-  ./scripts/run-docker.sh ./scripts/build-bootstraps.sh --add nodejs-lts,openjdk-17 --architectures aarch64
-
-  log "Building bootstrap for arm"
-  ./scripts/run-docker.sh ./scripts/build-bootstraps.sh --add nodejs-lts,openjdk-17 --architectures arm
-} | tee -a logs/build-bootstrap.log
 
 {
   clear
