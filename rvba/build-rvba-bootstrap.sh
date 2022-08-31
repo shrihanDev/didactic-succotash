08a9898ceffa1d6a8b1ad16e1cf86d02d7376593
shrihanDev <shrihankp198725@gmail.com>
2022-08-31 23:17:34 +0530

    mod rvba script
    Signed-off-by: shrihanDev <shrihankp198725@gmail.com>

--- rvba/build-rvba-bootstrap.sh
+++ rvba/build-rvba-bootstrap.sh
@@ -24,12 +24,16 @@ log(){
 
   log "Patching generate script"
   curl -sLo scripts/generate-bootstraps.sh https://github.com/termux/termux-packages/raw/60a48180ed2e316404e1913fd8b6f382520cb333/scripts/generate-bootstraps.sh
+  PKGS_TO_REMOVE='${TERMUX_PACKAGE_MANAGER} bzip2 command-not-found proot diffutils findutils gawk grep less procps psmisc sed tar termux-keyring util-linux xz-utils'
+  for pkg in $PKGS_TO_REMOVE; do
+    sed -i "s@pull_package $pkg@# $pkg, take a huge L@g" ./scripts/generate-bootstraps.sh
+  done
 
   log "Building packages for aarch64"
-  ./scripts/run-docker.sh ./build-package.sh -a aarch64 bash dash coreutils nano unzip nodejs-lts openjdk-17
+  ./scripts/run-docker.sh ./build-package.sh -a aarch64 bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
 
   log "Building packages for arm"
-  ./scripts/run-docker.sh ./build-package.sh -a arm bash dash coreutils nano nodejs-lts openjdk-17
+  ./scripts/run-docker.sh ./build-package.sh -a arm bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
 
   log "Building bootstrap for aarch64"
   ./scripts/run-docker.sh ./scripts/generate-bootstraps.sh --architectures aarch64 -c -i bash,dash,coreutils,nano,unzip,nodejs-lts,openjdk-17
