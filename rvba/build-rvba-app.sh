dad14dc7fc6e666ad41d7eb593353ca10f49beab
shrihanDev <shrihankp198725@gmail.com>
2022-09-01 07:56:04 +0530

    mod rvba wflow+script

--- rvba/build-rvba-app.sh
+++ rvba/build-rvba-app.sh
@@ -5,18 +5,17 @@ log(){
   echo -e "\e[34m[**] $1\e[0m" >&2
 }
 
-{
-  clear
+clear
 
-  log "Saving bootstrap zips"
-  cp $(find -name bootstrap-*.zip -type f) ~
+log "Saving bootstrap zips"
+cp $(find -name bootstrap-*.zip -type f) ~
 
-  log "Deleting bootstrap build cache"
-  cd ..
-  docker rm -vf $(docker ps -aq)
+log "Deleting bootstrap build cache"
+cd ..
+docker rm -vf $(docker ps -aq)
 
-  log "Cloning termux-app"
-  git clone --depth=1 --no-tags https://github.com/termux/termux-app
+log "Cloning termux-app"
+git clone --depth=1 --no-tags https://github.com/termux/termux-app
 
 #  log "Patching setup scripts"
 #  sed -i '/venv/d' termux-packages/scripts/setup-ubuntu.sh
@@ -31,31 +30,31 @@ log(){
 #  git reset HEAD termux-packages/scripts/setup-ubuntu.sh
 #  git reset HEAD termux-packages/scripts/setup-android-sdk.sh
 
-  cd termux-app
+cd termux-app
 
-  log "Changing package IDs"
-  git grep -l 'com.termux' | xargs sed -i 's#com.termux#com.reisxd.rvba#g'
-  git grep -l 'com_termux' | xargs sed -i 's#com_termux#com_reisxd_rvba#g'
+log "Changing package IDs"
+git grep -l 'com.termux' | xargs sed -i 's#com.termux#com.reisxd.rvba#g'
+git grep -l 'com_termux' | xargs sed -i 's#com_termux#com_reisxd_rvba#g'
 
-  log "Patching downloadBootstrap()"
-  sed -i '#def downloadBootstrap#a return;' app/build.gradle
+log "Patching downloadBootstrap()"
+sed -i '#def downloadBootstrap#a return;' app/build.gradle
 
-  log "Copying aarch64 bootstrap"
-  cp ~/bootstrap-aarch64.zip app/src/main/cpp
+log "Copying aarch64 bootstrap"
+cp ~/bootstrap-aarch64.zip app/src/main/cpp
 
-  log "Patching termux-bootstrap-zip.S"
-  cat <<EOF >app/src/main/cpp/termux-bootstrap-zip.S
+log "Patching termux-bootstrap-zip.S"
+cat <<EOF >app/src/main/cpp/termux-bootstrap-zip.S
 asm
-     .global blob
-     .global blob_size
-     .section .rodata
+   .global blob
+   .global blob_size
+   .section .rodata
  blob:
  #if defined __i686__
 
  #elif defined __x86_64__
 
  #elif defined __aarch64__
-     .incbin "bootstrap-aarch64.zip"
+   .incbin "bootstrap-aarch64.zip"
  #elif defined __arm__
 
  #else
@@ -63,28 +62,28 @@ asm
  #endif
  1:
  blob_size:
-     .int 1b - blob
+   .int 1b - blob
 EOF
-  
-  log "Patching app/build.gradle"
-  sed "s#include 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#include 'arm64-v8a'#g" app/build.gradle
 
-  log "Patching terminal-emulator/build.gradle"
-  sed "s#abiFilters 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#abiFilters 'arm64-v8a'#g"
+log "Patching app/build.gradle"
+sed "s#include 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#include 'arm64-v8a'#g" app/build.gradle
 
-  log "Building app for aarch64"
-  ./gradlew -a --daemon --parallel --build-cache --configuration-cache build
+log "Patching terminal-emulator/build.gradle"
+sed "s#abiFilters 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#abiFilters 'arm64-v8a'#g"
 
-  log "Copying arm bootstrap"
-  rm app/src/main/cpp/bootstrap-aarch64.zip
-  cp ~/bootstrap-arm.zip app/src/main/cpp
+log "Building app for aarch64"
+./gradlew -a --daemon --parallel --build-cache --configuration-cache build
 
-  log "Patching termux-bootstrap-zip.S"
-  cat <<EOF >app/src/main/cpp/termux-bootstrap-zip.S
+log "Copying arm bootstrap"
+rm app/src/main/cpp/bootstrap-aarch64.zip
+cp ~/bootstrap-arm.zip app/src/main/cpp
+
+log "Patching termux-bootstrap-zip.S"
+cat <<EOF >app/src/main/cpp/termux-bootstrap-zip.S
 asm
-     .global blob
-     .global blob_size
-     .section .rodata
+   .global blob
+   .global blob_size
+   .section .rodata
  blob:
  #if defined __i686__
 
@@ -93,22 +92,21 @@ asm
  #elif defined __aarch64__
 
  #elif defined __arm__
-     .incbin "bootstrap-arm.zip"
+   .incbin "bootstrap-arm.zip"
 
  #else
  # error Unsupported arch
  #endif
  1:
  blob_size:
-     .int 1b - blob
+   .int 1b - blob
 EOF
-  
-  log "Patching app/build.gradle"
-  sed "s#include 'arm64-v8a'#include 'armeabi-v7a'#g" app/build.gradle
 
-  log "Patching terminal-emulator/build.gradle"
-  sed "s#abiFilters 'arm64-v8a'#abiFilters 'armeabi-v7a'#g" terminal-emulator/build.gradle
+log "Patching app/build.gradle"
+sed "s#include 'arm64-v8a'#include 'armeabi-v7a'#g" app/build.gradle
 
-  log "Building app for arm"
-  ./gradlew -a --daemon --parallel --build-cache --configuration-cache build
-} | tee -a logs/build-app.log
+log "Patching terminal-emulator/build.gradle"
+sed "s#abiFilters 'arm64-v8a'#abiFilters 'armeabi-v7a'#g" terminal-emulator/build.gradle
+
+log "Building app for arm"
+./gradlew -a --daemon --parallel --build-cache --configuration-cache build
--- rvba/build-rvba-bootstrap.sh
+++ rvba/build-rvba-bootstrap.sh
@@ -5,40 +5,37 @@ log(){
   echo -e "\e[34m[**] $1\e[0m" >&2
 }
 
-{
-  log "Cloning termux-packages"
-  git clone https://github.com/termux/termux-packages --depth=1 --no-tags
-  cd termux-packages
+log "Cloning termux-packages"
+git clone https://github.com/termux/termux-packages --depth=1 --no-tags
+cd termux-packages
 
-  log "Changing package ID"
-  sed -i 's/TERMUX_APP_PACKAGE="com.termux"/TERMUX_APP_PACKAGE="com.reisxd.rvba"/g' scripts/properties.sh
+log "Changing package ID"
+sed -i 's/TERMUX_APP_PACKAGE="com.termux"/TERMUX_APP_PACKAGE="com.reisxd.rvba"/g' scripts/properties.sh
 
-  #log "Patching setup scripts"
-  #sed -i '/venv/d' scripts/setup-ubuntu.sh
-  #sed -i 's/openjdk-18/openjdk-17/g' scripts/setup-ubuntu.sh
-  #sed -i 's#cmdline-tools/bin#cmdline-tools/latest/bin#g' scripts/setup-android-sdk.sh
+#log "Patching setup scripts"
+#sed -i '/venv/d' scripts/setup-ubuntu.sh
+#sed -i 's/openjdk-18/openjdk-17/g' scripts/setup-ubuntu.sh
+#sed -i 's#cmdline-tools/bin#cmdline-tools/latest/bin#g' scripts/setup-android-sdk.sh
 
-  #log "Running setup scripts"
-  #./scripts/setup-ubuntu.sh
-  #./scripts/setup-android-sdk.sh
+#log "Running setup scripts"
+#./scripts/setup-ubuntu.sh
+#./scripts/setup-android-sdk.sh
 
-  log "Patching generate script"
-  curl -sLo scripts/generate-bootstraps.sh https://github.com/termux/termux-packages/raw/60a48180ed2e316404e1913fd8b6f382520cb333/scripts/generate-bootstraps.sh
-  PKGS_TO_REMOVE='${TERMUX_PACKAGE_MANAGER} bzip2 command-not-found proot diffutils findutils gawk grep less procps psmisc sed tar termux-keyring util-linux xz-utils'
-  for pkg in $PKGS_TO_REMOVE; do
-    sed -i "s@pull_package $pkg@# $pkg, take a huge L@g" ./scripts/generate-bootstraps.sh
-  done
+log "Patching generate script"
+curl -sLo scripts/generate-bootstraps.sh https://github.com/termux/termux-packages/raw/60a48180ed2e316404e1913fd8b6f382520cb333/scripts/generate-bootstraps.sh
+PKGS_TO_REMOVE='${TERMUX_PACKAGE_MANAGER} bzip2 command-not-found proot diffutils findutils gawk grep less procps psmisc sed tar termux-keyring util-linux xz-utils'
+for pkg in $PKGS_TO_REMOVE; do
+  sed -i "s@pull_package $pkg@# $pkg, take a huge L@g" ./scripts/generate-bootstraps.sh
+done
 
-  log "Building packages for aarch64"
-  ./scripts/run-docker.sh ./build-package.sh -a aarch64 bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
+log "Building packages for aarch64"
+./scripts/run-docker.sh ./build-package.sh -a aarch64 bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
 
-  log "Building packages for arm"
-  ./scripts/run-docker.sh ./build-package.sh -a arm bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
+log "Building packages for arm"
+./scripts/run-docker.sh ./build-package.sh -a arm bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
 
-  log "Building bootstrap for aarch64"
-  ./scripts/run-docker.sh ./scripts/generate-bootstraps.sh --architectures aarch64 -c --add nodejs-lts,openjdk-17
+log "Building bootstrap for aarch64"
+./scripts/run-docker.sh ./scripts/generate-bootstraps.sh --architectures aarch64 -c --add nodejs-lts,openjdk-17
 
-  log "Building bootstrap for arm"
-  ./scripts/run-docker.sh ./scripts/generate-bootstraps.sh --architectures arm -c --add nodejs-lts,openjdk-17
-} | tee -a logs/build-bootstrap.log
-
+log "Building bootstrap for arm"
+./scripts/run-docker.sh ./scripts/generate-bootstraps.sh --architectures arm -c --add nodejs-lts,openjdk-17
