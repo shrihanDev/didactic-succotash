672e718c9964862ce76dbe8b5918d40551e9709e
shrihanDev <shrihankp198725@gmail.com>
2022-09-01 21:18:54 +0530

    mod beef + rvba script

--- rvba/build-rvba-app.sh
+++ rvba/build-rvba-app.sh
@@ -7,16 +7,32 @@ log(){
 
 log "Cloning termux-app"
 git clone --depth=1 --no-tags https://github.com/termux/termux-app
+
+log "Cloning termux-packages"
+git clone --depth=1 --no-tags https://github.com/termux/termux-packages
+
+log "Patching setup scripts"
+sed -i 's/TERMUX_APP_PACKAGE="com.termux"/TERMUX_APP_PACKAGE="com.reisxd.rvba"/g' termux-packages/scripts/properties.sh
+sed -i '/venv/d' termux-packages/scripts/setup-ubuntu.sh
+sed -i 's/openjdk-18/openjdk-17/g' termux-packages/scripts/setup-ubuntu.sh
+sed -i 's#cmdline-tools/bin#cmdline-tools/latest/bin#g' termux-packages/scripts/setup-android-sdk.sh
+
+log "Running setup scripts"
+./termux-packages/scripts/setup-ubuntu.sh
+./termux-packages/scripts/setup-android-sdk.sh
+
+log "Deleting termux-packages"
+rm -rf termux-packages
 cd termux-app
 
 log "Changing package IDs"
 git grep -l 'com.termux' | xargs sed -i 's#com.termux#com.reisxd.rvba#g'
-git grep -l 'com_termux' | xargs sed -i 's#com_termux#com_reisxd_rvba#g'
+(git grep -l 'com_termux' | xargs sed -i 's#com_termux#com_reisxd_rvba#g') || true
 
 log "Patching downloadBootstrap()"
 sed -i '#def downloadBootstrap#a return;' app/build.gradle
 
-if [[ $1 == aarch64 ]] || [[ -z $1 ]]; then
+if [[ -z $1 ]] || [[ $1 == aarch64 ]]; then
 
   log "Copying aarch64 bootstrap"
   cp ~/bootstrap-aarch64.zip app/src/main/cpp
@@ -45,10 +61,10 @@ asm
 EOF
 
   log "Patching app/build.gradle"
-  sed "s#include 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#include 'arm64-v8a'#g" app/build.gradle
+  sed -i "s#include 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#include 'arm64-v8a'#g" app/build.gradle
 
   log "Patching terminal-emulator/build.gradle"
-  sed "s#abiFilters 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#abiFilters 'arm64-v8a'#g"
+  sed -i "s#abiFilters 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#abiFilters 'arm64-v8a'#g" terminal-emulator/build.gradle
 
   log "Building app for aarch64"
   ./gradlew -a --daemon --parallel --build-cache --configuration-cache build
@@ -84,10 +100,10 @@ asm
 EOF
 
   log "Patching app/build.gradle"
-  sed "s#include 'arm64-v8a'#include 'armeabi-v7a'#g" app/build.gradle
+  sed -i "s#include 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#include 'armeabi-v7a'#g" app/build.gradle
 
   log "Patching terminal-emulator/build.gradle"
-  sed "s#abiFilters 'arm64-v8a'#abiFilters 'armeabi-v7a'#g" terminal-emulator/build.gradle
+  sed -i "s#abiFilters 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#abiFilters 'armeabi-v7a'#g" terminal-emulator/build.gradle
 
   log "Building app for arm"
   ./gradlew -a --daemon --parallel --build-cache --configuration-cache build
