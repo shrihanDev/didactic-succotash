20874bd2865c14fd46b8073c6eacf3670882923f
shrihanDev <shrihankp198725@gmail.com>
2022-09-02 15:23:53 +0530

    mod rvba script

--- rvba/build-rvba-app.sh
+++ rvba/build-rvba-app.sh
@@ -3,41 +3,53 @@ set -euo pipefail
 
 log(){
   echo -e "\e[34m[**] $1\e[0m" >&2
+  [[ -z "$2" ]] && echo "::group::$1"
+}
+end_group(){
+  echo "::endgroup::"
 }
 
 log "Cloning termux-app"
 git clone --depth=1 --no-tags https://github.com/termux/termux-app
+end_group
 
 log "Cloning termux-packages"
 git clone --depth=1 --no-tags https://github.com/termux/termux-packages
+end_group
 
 log "Patching setup scripts"
 sed -i 's/TERMUX_APP_PACKAGE="com.termux"/TERMUX_APP_PACKAGE="com.reisxd.rvba"/g' termux-packages/scripts/properties.sh
 sed -i '/venv/d' termux-packages/scripts/setup-ubuntu.sh
 sed -i 's/openjdk-18/openjdk-17/g' termux-packages/scripts/setup-ubuntu.sh
 sed -i 's#cmdline-tools/bin#cmdline-tools/latest/bin#g' termux-packages/scripts/setup-android-sdk.sh
+end_group
 
 log "Running setup scripts"
 ./termux-packages/scripts/setup-ubuntu.sh
 ./termux-packages/scripts/setup-android-sdk.sh
+end_group
 
 log "Deleting termux-packages"
 rm -rf termux-packages
+end_group
 
 cd termux-app
 
 log "Changing package IDs"
-git grep -l 'com\.termux' | xargs sed -i 's#com\.termux#com\.reisxdw\.rvba#g'
+git grep -l 'com\.termux' | xargs sed -i 's#com\.termux#com\.reisxd\.rvba#g'
 (git grep -l 'com_termux' | xargs sed -i 's#com_termux#com_reisxd_rvba#g') || true
 sed -i 's#implementation "com\.reisxd\.rvba:termux-am-library:v2\.0\.0"#implementation "com\.termux:termux-am-library:v2\.0\.0"#g' termux-shared/build.gradle
+end_group
 
 log "Patching downloadBootstrap()"
 sed -i '#def downloadBootstrap#a return;' app/build.gradle
+end_group
 
 if [[ -z $1 ]] || [[ $1 == aarch64 ]]; then
 
   log "Copying aarch64 bootstrap"
   cp ~/bootstrap-aarch64.zip app/src/main/cpp
+  end_group
 
   log "Patching termux-bootstrap-zip.S"
   cat <<EOF >app/src/main/cpp/termux-bootstrap-zip.S
@@ -61,20 +73,25 @@ asm
  blob_size:
    .int 1b - blob
 EOF
+  end_group
 
   log "Patching app/build.gradle"
   sed -i "s#include 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#include 'arm64-v8a'#g" app/build.gradle
+  end_group
 
   log "Patching terminal-emulator/build.gradle"
   sed -i "s#abiFilters 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#abiFilters 'arm64-v8a'#g" terminal-emulator/build.gradle
+  end_group
 
   log "Building app for aarch64"
   ./gradlew -a --daemon --parallel --build-cache --configuration-cache assembleDebug
+  end_group
 
 elif [[ $1 == arm ]]; then
 
   log "Copying arm bootstrap"
   cp ~/bootstrap-arm.zip app/src/main/cpp
+  end_group
 
   log "Patching termux-bootstrap-zip.S"
   cat <<EOF >app/src/main/cpp/termux-bootstrap-zip.S
@@ -99,17 +116,21 @@ asm
  blob_size:
    .int 1b - blob
 EOF
+  end_group
 
   log "Patching app/build.gradle"
   sed -i "s#include 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#include 'armeabi-v7a'#g" app/build.gradle
+  end_group
 
   log "Patching terminal-emulator/build.gradle"
   sed -i "s#abiFilters 'x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'#abiFilters 'armeabi-v7a'#g" terminal-emulator/build.gradle
+  end_group
 
   log "Building app for arm"
   ./gradlew -a --daemon --parallel --build-cache --configuration-cache assembleDebug
+  end_group
 
 else
-  log "Invalid arch: $1"
+  log "Invalid arch: $1" n
   exit 128
 fi
--- rvba/build-rvba-bootstrap.sh
+++ rvba/build-rvba-bootstrap.sh
@@ -3,14 +3,20 @@ set -euo pipefail
 
 log(){
   echo -e "\e[34m[**] $1\e[0m" >&2
+  [[ -z "$2" ]] && echo "::group::$1"
+}
+end_group(){
+  echo "::endgroup::"
 }
 
 log "Cloning termux-packages"
 git clone https://github.com/termux/termux-packages --depth=1 --no-tags
 cd termux-packages
+end_group
 
 log "Changing package ID"
 sed -i 's/TERMUX_APP_PACKAGE="com.termux"/TERMUX_APP_PACKAGE="com.reisxd.rvba"/g' scripts/properties.sh
+end_group
 
 log "Patching generate script"
 curl -sLo scripts/generate-bootstraps.sh https://github.com/termux/termux-packages/raw/60a48180ed2e316404e1913fd8b6f382520cb333/scripts/generate-bootstraps.sh
@@ -20,20 +26,25 @@ for pkg in $PKGS_TO_REMOVE; do
 done
 sed -i "174ifi" ./scripts/generate-bootstraps.sh
 sed -i "227d" ./scripts/generate-bootstraps.sh
+end_group
 
 if [[ $1 == aarch64 ]] || [[ -z $1 ]]; then
   log "Building packages for aarch64"
   ./scripts/run-docker.sh ./build-package.sh -a aarch64 bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
+  end_group
 
   log "Building bootstrap for aarch64"
   ./scripts/run-docker.sh ./scripts/generate-bootstraps.sh --architectures aarch64 -c --add nodejs-lts,openjdk-17
+  end_group
 elif [[ $1 == arm ]]; then
   log "Building packages for arm"
   ./scripts/run-docker.sh ./build-package.sh -a arm bash coreutils dash termux-exec termux-tools nano unzip nodejs-lts openjdk-17
+  end_group
 
   log "Building bootstrap for arm"
   ./scripts/run-docker.sh ./scripts/generate-bootstraps.sh --architectures arm -c --add nodejs-lts,openjdk-17
+  end_group
 else
-  log "Invalid arch: $1"
+  log "Invalid arch: $1" n
   exit 127
 fi
